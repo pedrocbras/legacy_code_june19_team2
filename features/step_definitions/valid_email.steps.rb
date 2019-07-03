451ba6978a6f6725d9784920f0e594a94ef63c27
pedro_bras <pedro.c.bras@gmail.com>
2019-07-03 17:48:29 +0200

    adding tests for invalid email error, all tests green

--- features/step_definitions/valid_email.steps.rb
+++ features/step_definitions/valid_email.steps.rb
@@ -1,8 +1,19 @@
-Given("a invalid email is inserted") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-Then("i should get a error message") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+Given("I visit the site") do
+  visit root_path
+end
 
+Then("I should see {string}") do |content|
+  expect(page).to have_content(content)
+end
+
+When("I click {string}") do |button|
+  click_on(button)
+end
+
+When("I fill in {string} with {string}") do |field, content|
+  fill_in(field, :with => content)
+end
+
+Then("i should see {string}") do |error|
+  expect(page).to have_content(error)
+end
--- features/valid_email.feature
+++ features/valid_email.feature
@@ -4,5 +4,12 @@ Feature: Check for a valid email
     I need to have a valid email
 
 Scenario: If user email is not valid
-    Given a invalid email is inserted
-    Then i should get a error message
+    Given I visit the site
+    Then I should see "Sign up"
+    When I click "Sign up"
+    Then I fill in "Name" with "John Doe"
+    And I fill in "Email" with "johndoe"
+    And I fill in "Password" with "password1"
+    And I fill in "Password confirmation" with "password1"
+    And I click "Create"
+    Then i should see "Email is invalid"
