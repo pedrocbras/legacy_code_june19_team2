cbc23b5cdf231e7f2bea0b18154c1a3c0e8ffe6b
pedro_bras <pedro.c.bras@gmail.com>
2019-07-03 13:45:55 +0200

    adds root path to feature file, first test passing

--- features/step_definitions/create_new_account_steps.rb
+++ features/step_definitions/create_new_account_steps.rb
@@ -1,5 +1,5 @@
 Given("I visit the site") do
-    pending # Write code here that turns the phrase above into concrete actions
+    visit root_path
   end
   
   Then("I should see {string}") do |string|
