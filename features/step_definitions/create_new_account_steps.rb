9aa90380e321516fdcc75b2127acb5e87f692b83
pedro_bras <pedro.c.bras@gmail.com>
2019-07-03 16:55:02 +0200

    fixing last test - all green

--- features/step_definitions/create_new_account_steps.rb
+++ features/step_definitions/create_new_account_steps.rb
@@ -2,8 +2,8 @@ Given("I visit the site") do
     visit root_path
   end
   
-  Then("I should see {string}") do |string|
-    expect(page).to have_content "Sign up"
+  Then("I should see {string}") do |content|
+    expect(page).to have_content(content)
   end
 
   When("I click  {string} link") do |button|
