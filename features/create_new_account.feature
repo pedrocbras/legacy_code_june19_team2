43222f416c79dc0aaba845f2840184f53f8122a2
pedro_bras <pedro.c.bras@gmail.com>
2019-07-03 14:03:37 +0200

    adds full sign up feature story to feature file and code blocks to steps file

--- features/create_new_account.feature
+++ features/create_new_account.feature
@@ -7,6 +7,13 @@ Feature: Creating new user
   Scenario: Create new account
     Given I visit the site
     Then I should see "Sign up"
+    When I click  "Sign up" link
+    Then I fill in "Name" with "John Doe"
+    And I fill in "Email" with "johndoe@gmail.com"
+    And I fill in "Password" with "password1"
+    And I fill in "Password confirmation" with "password1"
+    And I click "Create"
+    Then I should see "Welcome! You have signed up successfully."
     
 
 
--- features/step_definitions/create_new_account_steps.rb
+++ features/step_definitions/create_new_account_steps.rb
@@ -5,4 +5,16 @@ Given("I visit the site") do
   Then("I should see {string}") do |string|
     expect(page).to have_content "Sign up"
   end
+
+  When("I click  {string} link") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I fill in {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I click {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
   
