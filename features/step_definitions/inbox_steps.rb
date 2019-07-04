cd4b45b3fbd5694c7e1c42ed946d3687105a0a89
Seth NeJame <sethnejame@gmail.com>
2019-07-04 15:18:38 +0200

    See Login Button and Click on Login Button Passing

--- features/step_definitions/inbox_steps.rb
+++ features/step_definitions/inbox_steps.rb
@@ -8,12 +8,12 @@ Given("I visit the site") do
   visit(root_path)
 end
 
-Then("I should see {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I should see {string}") do |content|
+  expect(page).to have_content content
 end
 
-When("I click {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I click {string}") do |button|
+  click_on button
 end
 
 When("I enter {string} into the {string} field") do |string, string2|
