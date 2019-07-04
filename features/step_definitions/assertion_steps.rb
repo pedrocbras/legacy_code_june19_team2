abf430291c7d7d3fef3ebe6ddf70cdb36401c2d1
pedro_bras <pedro.c.bras@gmail.com>
2019-07-04 16:18:21 +0200

    minor corrections and refactoring

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -1,3 +1,7 @@
 Then("I fill {string} with {string}") do |field, content|
     fill_in(field, with: content)
 end
+
+Then("I should see {string}") do |content|
+    expect(page).to have_content(content)
+  end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,15 +1,11 @@
   Given("I visit the site") do
     visit root_path
   end
-  
-  Then("I should see {string}") do |content|
-    expect(page).to have_content(content)
-  end
 
   When("I click {string} link") do |button|
     click_on button
   end
   
-  Then("I click {string}") do |button|
+  When("I click {string}") do |button|
     click_on button
   end
