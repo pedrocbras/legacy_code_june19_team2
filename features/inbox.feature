35e172c771ed32f29a2d520ff8a7341ed648d0cb
Seth NeJame <sethnejame@gmail.com>
2019-07-04 15:09:00 +0200

    Background Test Req. Working (FactoryBot user & visit site) and green, moving on with inbox features

--- features/inbox.feature
+++ features/inbox.feature
@@ -7,9 +7,10 @@ Background:
     Given the following user exists
         | email              |  password  | name       |
         | hackerman@gmail.com|  hackerman |  Hackerman |
-    And I visit the site
+    Given I visit the site
 
   Scenario: View Inbox when User Logs In
+    Then I should see "Login"
     When I click "Login"
     And I enter "hackerman@gmail.com" into the "Email" field
     And I enter "hackerman" into the "Password" field
@@ -20,11 +21,11 @@ Background:
     And I should see "No messages yet" in my inbox area
     
 
-  Scenario: Send Messages to another user
-    When I click on "Compose" in my Inbox
-    Then I should see "Recipients" and "Subject" and "Message"
-    And I enter "Bob" into the "Recipients" field
-    And I enter "Hello!" into the "Subject" field
-    And I enter "This is a test message" into the "Message" field
-    And I click on the "Send Message" button
-    Then I should see "Your message was successfully sent!"
+  # Scenario: Send Messages to another user
+  #   When I click on "Compose" in my Inbox
+  #   Then I should see "Recipients" and "Subject" and "Message"
+  #   And I enter "Bob" into the "Recipients" field
+  #   And I enter "Hello!" into the "Subject" field
+  #   And I enter "This is a test message" into the "Message" field
+  #   And I click on the "Send Message" button
+  #   Then I should see "Your message was successfully sent!"
--- features/step_definitions/inbox_steps.rb
+++ features/step_definitions/inbox_steps.rb
@@ -1,10 +1,14 @@
-Given("the following user exists:") do |table|
+Given("the following user exists") do |table|
   table.hashes.each do |table|
-    Factory.Bot.create(:user, table)
+    FactoryBot.create(:user, table)
   end
 end
-  
+
 Given("I visit the site") do
+  visit(root_path)
+end
+
+Then("I should see {string}") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -13,10 +17,6 @@ When("I click {string}") do |string|
 end
 
 When("I enter {string} into the {string} field") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I should see {string}") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -29,21 +29,5 @@ Then("I should see a {string} and {string} and {string} and {string}") do |strin
 end
 
 Then("I should see {string} in my inbox area") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I click on {string} in my Inbox") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I should see {string} and {string} and {string}") do |string, string2, string3|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I enter {string} into the {string} field") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I click on the {string} button") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
