4a34d271a1b846b8df6800a30fb3e321a7a6d229
Seth NeJame <sethnejame@gmail.com>
2019-07-05 15:29:38 +0200

    Removing comments per Faraz' request

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
   factory :user do
-    # name { "MyString" }
-    # email { "MyString" }
-    # password { "MyString" }
     password {'mypassword'}
     sequence(:name) { |n| "user#{n}" }
     sequence(:email) { |n| "email#{n}@.com" }
