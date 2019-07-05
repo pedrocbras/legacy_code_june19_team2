c03500fb356f7db9b4e09e5c29ac372a94abb256
Daria Lykova <daria.lykova@gmail.com>
2019-07-05 10:07:20 +0200

    refactors steps

--- features/step_definitions/log_in_steps.rb
+++ features/step_definitions/log_in_steps.rb
@@ -3,24 +3,4 @@ Given("the following user exists") do |table|
        FactoryBot.create(:user, user)
     end
   end
-
-  Given("I visit the site") do
-    visit(root_path)
-  end
-  
-  Then("I should see {string}") do |button|
-    expect(page).to have_content(button)
-  end
-  
-  When("I click {string}") do |element|
-    click_on(element)
-  end
-  
-  Then("I fill in {string} with {string}") do |field, input|
-    fill_in field, with: input
-  end
-  
-  Then("i should see {string}") do |content|
-    expect(page).to have_content(content)
-  end
-  
+    
