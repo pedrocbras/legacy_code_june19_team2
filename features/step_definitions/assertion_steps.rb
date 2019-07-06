d6887e2aec940dce9e88deb0a4c68fb0b99849aa
Daria Lykova <daria.lykova@gmail.com>
2019-07-06 11:58:13 +0200

    Modifies the code syntax in order to pass the 'I should see '1' message' test

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -2,7 +2,7 @@ Then("I should see {string}") do |content|
     expect(page).to have_content(content)
 end
 
-Then("I should see {string} message") do |expected_count|
-    count = @receiver.mailbox.inbox.count
-    expect(count).to expected_count.to_i 
+Then(/^I should see "([^"]*)" message$/) do |expected_count|
+  count = @receiver.mailbox.inbox.count
+  expect(count).to eq expected_count.to_i
 end
